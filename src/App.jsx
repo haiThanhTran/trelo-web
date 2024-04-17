ca5ee21cd11f4a57199d48c11aed9b8d4052b068
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-17 19:35:41 +0700

    config_dark-light

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import HomeIcon from "@mui/icons-material/Home"
-import { pink } from "@mui/material/colors"
-import Typography from "@mui/material/Typography"
-import {useColorScheme} from "@mui/material/styles"
-import Button from "@mui/material/Button"
+import HomeIcon from "@mui/icons-material/Home";
+import { pink } from "@mui/material/colors";
+import Typography from "@mui/material/Typography";
+import { useColorScheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    console.log(selectedMode);
+    setMode(selectedMode);
+    // setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const prefersLightMode = useMediaQuery("(prefers-color-scheme: light)");
+  console.log("preferencesDarkMode:", prefersDarkMode);
+  console.log("preferencesLightMode:", prefersLightMode);
+
   const { mode, setMode } = useColorScheme();
   return (
     <Button
@@ -23,11 +76,11 @@ function ModeToggle() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <ModeToggle/>
+      <ModeSelect />
+      <hr />
+      <ModeToggle />
       <hr />
       <div>haiThanhTran</div>
       <Typography variant="body2" color="text.secondary">
