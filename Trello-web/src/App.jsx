4772af38972c17a9c39ab95ef3bc50e431ea03a0
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-17 08:53:57 +0700

    config_theme

--- Trello-web/src/App.jsx
+++ Trello-web/src/App.jsx
@@ -5,6 +5,7 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
 import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
+import Typography from "@mui/material/Typography";
 
 import Button from "@mui/material/Button";
 function App() {
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <div>haiThanhTran</div>
+      <Typography variant="body2" color="text.secondary">Hai</Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
