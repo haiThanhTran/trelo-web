8176684f54f75fbff37730702dc5bf8c8678cad3
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-17 15:04:45 +0700

    config_dark-light_mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import HomeIcon from "@mui/icons-material/Home";
-import { pink } from "@mui/material/colors";
+import HomeIcon from "@mui/icons-material/Home"
+import { pink } from "@mui/material/colors"
+import Typography from "@mui/material/Typography"
+import {useColorScheme} from "@mui/material/styles"
+import Button from "@mui/material/Button"
 
-import Button from "@mui/material/Button";
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <ModeToggle/>
+      <hr />
       <div>haiThanhTran</div>
+      <Typography variant="body2" color="text.secondary">
+        Hai
+      </Typography>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
