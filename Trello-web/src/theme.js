617a65b34f8f1a9ca0e89c7e1be1dfcab902f69c
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-18 20:01:31 +0700

    Layout_Basic

--- Trello-web/src/theme.js
+++ Trello-web/src/theme.js
@@ -1,22 +1,24 @@
 import { createTheme } from "@mui/material/styles";
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { deepOrange, teal, orange, cyan } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello:{
+    appBarHeight:'48px',
+    boardBarHeight:'58px'
+  },
   colorSchemes: {
     light: {
       palette: {
-        // primary: {
-        //   main: "#ff5252"
-        // }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        // primary: {
-        //   main: "#000"
-        // }
+        primary: cyan,
+        secondary: orange
       }
     }
   }
