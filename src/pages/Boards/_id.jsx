c82f50f89972830ab1270d17111195b9f199c881
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-19 09:43:37 +0700

    Refactor Project Structure

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/system";
+import Container from "@mui/material/Container";
+import AppBar from "../../components/AppBar";
+import BoardBar from "../Boards/BoardBar";
+import BoardContent from "../Boards/BoardContent";
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  );
+}
+export default Board;
