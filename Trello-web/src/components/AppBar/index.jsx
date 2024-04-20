d28c92f27087264a08fe1b2d446262af341af795
haiThanhTran <tranthanhhaivlogs@gmail.com>
2024-04-20 09:27:57 +0700

    app_bar_p2

--- Trello-web/src/components/AppBar/index.jsx
+++ Trello-web/src/components/AppBar/index.jsx
@@ -35,6 +35,7 @@ function AppBar() {
         <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
           <SvgIcon
             component={TrelloIcon}
+            fontSize='small'
             inheritViewBox
             sx={{ color: "primary.main" }}
           />
@@ -69,14 +70,14 @@ function AppBar() {
         />
         <ModeSelect />
 
-        <Tooltip title="Notification">
+        <Tooltip title="Notifications">
           <Badge color="secondary" variant="dot" sx={{cursor:'pointer'}}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{color:'primary.main'}}/>
           </Badge>
         </Tooltip>
 
         <Tooltip title="Help">
-            <HelpOutlineIcon sx={{cursor:'pointer'}}/>
+            <HelpOutlineIcon sx={{cursor:'pointer',color:'primary.main'}}/>
           
         </Tooltip>
         
